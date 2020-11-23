580cab74dc55970c50f1773a6395c553b48e7874
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2020-11-23 16:13:37 -0500

    Re-styling education section

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -8,25 +8,39 @@ export class Education extends Component {
     return (
       <div className="education">
         <div className="gmu">
-          <div className="gmu__info">
-            <h1>George Mason University</h1>
-            <h2>Information Technology</h2>
-            <h3>Bachelors of Science</h3>
-            <h4>2017-2020</h4>
+          <div className="gmu--logo">
+            <img src={GMU} alt="gmu--logo" className="gmu--img" />
           </div>
-          <div className="gmu__logo">
-            <img src={GMU} alt="GMU_logo" className="gmu__img" />
+          <div className="gmu--info">
+            <div className="gmu--text">
+              <h1>George Mason University</h1>
+              <h2>Information Technology</h2>
+              <h3>Bachelors of Science</h3>
+              <h4>2017-2020</h4>
+            </div>
+            <div className="gmu--website">
+              <a href="#" className="gmu--link">
+                Visit Site
+              </a>
+            </div>
           </div>
         </div>
         <div className="nova">
-          <div className="nova__logo">
-            <img src={NOVA} alt="NOVA_logo" className="nova__img" />
+          <div className="nova--logo">
+            <img src={NOVA} alt="gmu--logo" className="nova--img" />
           </div>
-          <div className="nova__info">
-            <h1>Northern Virginia Community College</h1>
-            <h2>Applied Computer Science</h2>
-            <h3>Associates</h3>
-            <h4>2013-2016</h4>
+          <div className="nova--info">
+            <div className="nova--text">
+              <h1>Northern Virginia Community College</h1>
+              <h2>Applied Computer Science</h2>
+              <h3>Associates</h3>
+              <h4>2013-2016</h4>
+            </div>
+            <div className="nova--website">
+              <a href="#" className="nova--link">
+                Visit Site
+              </a>
+            </div>
           </div>
         </div>
       </div>
