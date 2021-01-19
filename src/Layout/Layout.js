b5547db9e950f6a0363b0e648846b1b382921d3f
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2021-01-18 23:31:10 -0500

    Added Layout

---
+++ src/Layout/Layout.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import Main from "../pages/Main";
+import Experience from "../pages/Experience";
+import Education from "../pages/Education";
+import Projects from "../pages/Projects";
+
+function Layout() {
+  return (
+    <>
+      <Navigation />
+      <Main />
+      <Experience />
+      <Projects />
+      <Education />
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
