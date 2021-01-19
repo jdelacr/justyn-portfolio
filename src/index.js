356792f8a9a533a8fa0f9df9d70cad9964ef9c3f
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2021-01-18 23:31:28 -0500

    Implemented React router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import Routes from "./Routes";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
   document.getElementById("root")
 );
